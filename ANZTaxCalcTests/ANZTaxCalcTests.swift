a42ee1b9ad616d18f7b17399a888385b4e30d97d
Tran Quynh Tran <tran@trans-mbp.hub>
2018-06-13 23:04:48 +1200

    Implement unit test for InputAnnualSalaryNZ

--- ANZTaxCalcTests/ANZTaxCalcTests.swift
+++ ANZTaxCalcTests/ANZTaxCalcTests.swift
@@ -10,10 +10,14 @@ import XCTest
 @testable import ANZTaxCalc
 
 class ANZTaxCalcTests: XCTestCase {
+    var frequencyList = [Int: PayFrequency]()
+    let salary = 92000.00
+    let taxBucketNZ = TaxBucketNZ()
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -31,6 +35,26 @@ class ANZTaxCalcTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testInputAnnualSalaryNZ() {
+        var incomeNZ = IncomeNZ(taxBucket: taxBucketNZ, salary: Double(salary), inputPayFrequency: .Annually, outputPayFrequency: .Annually)
+        
+        XCTAssertEqual(salary, incomeNZ.inputAnnualSalary)
+        
+        incomeNZ = IncomeNZ(taxBucket: taxBucketNZ, salary: Double(salary/12), inputPayFrequency: .Monthly, outputPayFrequency: .Annually)
+        
+        XCTAssertEqual(salary, incomeNZ.inputAnnualSalary)
+        
+        incomeNZ = IncomeNZ(taxBucket: taxBucketNZ, salary: salary/Double(Date().daysPerYear())*7, inputPayFrequency: .Weekly, outputPayFrequency: .Annually)
+        
+        XCTAssertEqual(salary, incomeNZ.inputAnnualSalary)
+        
+        incomeNZ = IncomeNZ(taxBucket: taxBucketNZ, salary: Double(salary), inputPayFrequency: .Daily, outputPayFrequency: .Annually)
+        
+        XCTAssertEqual(salary, incomeNZ.inputAnnualSalary)
+        
+        incomeNZ = IncomeNZ(taxBucket: taxBucketNZ, salary: Double(salary), inputPayFrequency: .Hourly, outputPayFrequency: .Annually)
     }
     
 }
